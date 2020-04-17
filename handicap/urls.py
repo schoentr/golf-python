aacf74e9ac91fa373a405bca8a9b896d4d9cad49
Tim Schoen <schoentr@gmail.com>
2020-04-16 17:35:27 -0700

    tees  are rendering

--- handicap/urls.py
+++ handicap/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import course_list_view, course_detail_view
+from .views import course_list_view, user_handicap_view,tee_selection_view
 
 
 urlpatterns = [
+  path('user_handicap/',user_handicap_view, name='user_handicap'),
+  path('courses/',course_list_view, name='course_list_view'),
+  path('courses/<int:pk>',tee_selection_view,  name='tee_selection')
 
-  path('',course_list_view,name='course_list_view'),
-  path('<int:pk>',course_detail_view,name='course_detail_view'),
 ]
--- handicap/views.py
+++ handicap/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Course, Tee
 
 
 # Create your views here
+def home_view (request):
+  return render(request, 'generic/home.html' ,context={'message':'Tim'})
 
-def course_list_view(request):
-  courses = get_list_or_404(course)
-  context={
-    'courses':'courses'
-  }
-  return render(request, ' course/course_list.html',context)
 
-def course_detail_view(request, pk=none):
+def user_handicap_view (request):
+  name='tim'
   context = {
-    'course':get_object_or_404(course,  id=pk)
+    'message':name,
+
   }
-  return render (request, 'courses/course_detail.html', context)
+  return render(request, 'handicap/user_handicap.html' ,context )
+
+def course_list_view (request):
+  courses=  get_list_or_404(Course)
+  context= {
+    'courses':courses,
+  }
+  return  render (request, 'handicap/courses.html', context)
+
+def tee_selection_view (request, pk=None):
+  course = get_object_or_404(Course, id=pk)
+  tee = get_list_or_404(Tee, course_id=pk)
+  context= {
+    'course': course,
+    'tees':tee,
+  }
+  return render(request,'handicap/tee.html',  context)
+# def course_list_view(request):
+#   # courses = get_list_or_404(course)
+#   context={
+#     'courses':'courses'
+#   }
+#   return render(request, 'handicap/course_list.html',context)
+
